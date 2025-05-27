6d7db182ff273662adda997cb66b82826ca00578
Daniil Quix <daniil@quix.io>
2025-05-27 13:18:08 +0000

    Edited 'main.py'

--- enrichment/main.py
+++ enrichment/main.py
@@ -17,6 +17,7 @@ output_topic = app.topic(os.environ["output"])
 data_sdf = app.dataframe(input_data_topic)
 config_sdf = app.dataframe(input_config_topic)
 
+data_sdf.join_asof(config_sdf)
 
 # Create nice JSON alert message.
 data_sdf = data_sdf.apply(lambda row: {
