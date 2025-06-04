e13705f2ba2c3ca0b7c54c67e4ca7efc5a92287e
Steve Rosam <steve+newdemo@quix.io>
2025-06-04 09:12:37 +0000

    Edited 'main.py'

--- enrichment/main.py
+++ enrichment/main.py
@@ -29,16 +29,16 @@ def on_merge(left: dict, right: dict):
     right['timestamp'] = str(datetime.fromtimestamp(right['timestamp']/1000/1000/1000))
     return {"timestamp": date_str, "data": left, "config": right}
 
-# data_sdf = data_sdf.apply(lambda row: row, metadata=True)
-def groupby_custom(message):
-    print("=========================")
-    # print(message)
-    return message["location_id"]
+# # data_sdf = data_sdf.apply(lambda row: row, metadata=True)
+# def groupby_custom(message):
+#     print("=========================")
+#     # print(message)
+#     return message["location_id"]
 
 
-data_sdf = data_sdf.group_by(groupby_custom, name="unique_name")
+# data_sdf = data_sdf.group_by(groupby_custom, name="unique_name")
+data_sdf = data_sdf.group_by("location_id")
 data_sdf.print(metadata=True)
-# data_sdf = data_sdf.group_by("new_key")
 
 # Join the latest effective config with the data
 # data_sdf = data_sdf.join_asof(config_sdf, on_merge=on_merge)
