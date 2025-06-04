a2797d0d5cdb567c3d20b1d81759e3c2f74d86f5
Steve Rosam <steve+newdemo@quix.io>
2025-06-04 09:27:53 +0000

    Edited 'main.py'

--- enrichment/main.py
+++ enrichment/main.py
@@ -34,8 +34,8 @@ def on_merge(left: dict, right: dict):
 data_sdf = data_sdf.group_by("location_id")
 
 print("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*")
-data_sdf.print(metadata=True)
-config_sdf.print(metadata=True)
+# data_sdf.print(metadata=True)
+# config_sdf.print(metadata=True)
 print("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*")
 
 # Join the latest effective config with the data
