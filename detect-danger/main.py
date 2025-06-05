43db1c05012f98dc0a6ac56aaba1bc28a9bf6844
Steve Rosam <steve+newdemo@quix.io>
2025-06-05 15:30:15 +0000

    Edited 'main.py'

--- detect-danger/main.py
+++ detect-danger/main.py
@@ -6,35 +6,41 @@ from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="danger-v3.5", 
-                auto_offset_reset="earliest", 
+app = Application(consumer_group='danger-v3.5', 
+                auto_offset_reset='earliest', 
                 use_changelog_topics=False)
 
-input_topic = app.topic(os.environ["input"])
-output_topic = app.topic(os.environ["output"])
+input_topic = app.topic(os.environ['input'])
+output_topic = app.topic(os.environ['output'])
 
 sdf = app.dataframe(input_topic)
 
 # Filter items out without data and config values.
-sdf = sdf[sdf.contains("data")]
-sdf = sdf[sdf.contains("configuration")]
+sdf = sdf[sdf.contains('data')]
+sdf = sdf[sdf.contains('configuration')]
 
 def check_for_danger(row):
     # print(row)
-    panel_temp = float(row["data"]["temperature"])
+    panel_temp = float(row['data']['temperature'])
 
-    if "temperature" in row["configuration"]:
-        forecast_temp = float(row["configuration"]["temperature"])
-        forecast_cloud = float(row["configuration"]["cloud_cover"])
+    if 'temperature' in row['configuration']:
+        forecast_temp = float(row['configuration']['temperature'])
+        forecast_cloud = float(row['configuration']['cloud_cover'])
     else:
         return {}
 
     if panel_temp > 25 and forecast_temp > 40 and forecast_cloud < 40:
-        row["danger"] = True
+        row['danger'] = True
     else:
-        row["danger"] = False
+        row['danger'] = False
 
-    return row
+    return {
+        'timestamp': row['timestamp'],
+        'danger_detected': row['danger'],
+        'panel_temperature': row['data']['temperature'],
+        'panel_id': row['data']['panel_id'],
+        'forecast_temperature': row['configuration']['temperature']
+    }
 
 
 
@@ -42,16 +48,16 @@ def check_for_danger(row):
 sdf = sdf.apply(check_for_danger)
 #         .hopping_window(1000, 200).mean().final() 
 
-sdf = sdf[sdf.contains("data")]
+# sdf = sdf[sdf.contains('data')]
 
-# sdf.apply(lambda row: print(row["danger"]))
+# sdf.apply(lambda row: print(row['danger']))
 
 # Filter only windows where danger is True
-# sdf = sdf[sdf["danger"] == True]
+# sdf = sdf[sdf['danger'] == True]
 sdf.print()
 
 # Send the message to the output topic
 # sdf.to_topic(output_topic)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
