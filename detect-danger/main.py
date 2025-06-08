211f028eb51bd5b840738816c3f6737c0fae82cc
Merlin The Great <merlin+newdemo@quix.io>
2025-06-08 14:05:56 +0000

    adding starter transformation to 'main.py'

--- detect-danger/main.py
+++ detect-danger/main.py
@@ -1,61 +1,49 @@
-import os
+# import the Quix Streams modules for interacting with Kafka.
+# For general info, see https://quix.io/docs/quix-streams/introduction.html
 from quixstreams import Application
-from datetime import datetime
+
+import os
 
 # for local dev, load env vars from a .env file
-from dotenv import load_dotenv
-load_dotenv()
-
-app = Application(consumer_group='danger-v3.5', 
-                auto_offset_reset='earliest', 
-                use_changelog_topics=False)
-
-input_topic = app.topic(os.environ['input'])
-output_topic = app.topic(os.environ['output'])
-
-sdf = app.dataframe(input_topic)
-
-# Filter items out without data and config values.
-sdf = sdf[sdf.contains('data')]
-sdf = sdf[sdf.contains('configuration')]
-
-def check_for_danger(row):
-    # print(row)
-    panel_temp = float(row['data']['temperature'])
-
-    if 'temperature' in row['configuration']:
-        forecast_temp = float(row['configuration']['temperature'])
-        forecast_cloud = float(row['configuration']['cloud_cover'])
-    else:
-        return {}
-
-    if panel_temp > 25 and forecast_temp > 26.5 and forecast_cloud < 50:
-        row['danger'] = True
-    else:
-        row['danger'] = False
-
-    return {
-        'timestamp': row['timestamp'],
-        'danger_detected': row['danger'],
-        'panel_temperature': row['data']['temperature'],
-        'panel_id': row['data']['panel_id'],
-        'forecast_temperature': row['configuration']['temperature']
-    }
+# from dotenv import load_dotenv
+# load_dotenv()
 
 
+def main():
+    """
+    Transformations generally read from, and produce to, Kafka topics.
 
-# Calculate hopping window of 1s with 200ms steps.
-sdf = sdf.apply(check_for_danger)
-#         .hopping_window(1000, 200).mean().final() 
+    They are conducted with Applications and their accompanying StreamingDataFrames
+    which define what transformations to perform on incoming data.
 
+    Be sure to explicitly produce output to any desired topic(s); it does not happen
+    automatically!
 
-sdf.print()
-sdf = sdf[sdf.contains('danger_detected')]
-# Filter only windows where danger is True
-sdf = sdf[sdf['danger_detected'] == True]
+    To learn about what operations are possible, the best place to start is:
+    https://quix.io/docs/quix-streams/processing.html
+    """
 
-# Send the message to the output topic
-sdf.to_topic(output_topic)
+    # Setup necessary objects
+    app = Application(
+        consumer_group="my_transformation",
+        auto_create_topics=True,
+        auto_offset_reset="earliest"
+    )
+    input_topic = app.topic(name=os.environ["input"])
+    output_topic = app.topic(name=os.environ["output"])
+    sdf = app.dataframe(topic=input_topic)
 
-if __name__ == '__main__':
+    # Do StreamingDataFrame operations/transformations here
+    sdf = sdf.apply(lambda row: row).filter(lambda row: True)
+    sdf = sdf.print(metadata=True)
+
+    # Finish off by writing to the final result to the output topic
+    sdf.to_topic(output_topic)
+
+    # With our pipeline defined, now run the Application
     app.run()
+
+
+# It is recommended to execute Applications under a conditional main
+if __name__ == "__main__":
+    main()
